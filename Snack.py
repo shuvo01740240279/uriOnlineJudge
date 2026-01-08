a8760c5b64fef476e58069323a69ba004ee50cc0
shuvo01740240279 <belayethosenshuvo9@gmail.com>
2026-01-08 16:18:53 +0600

    Snack done

---
+++ Snack.py
@@ -0,0 +1,17 @@
+
+x, y = map(int, input().split())
+
+if x == 1:
+    price = 4.00
+elif x == 2:
+    price = 4.50
+elif x == 3:
+    price = 5.00
+elif x == 4:
+    price = 2.00
+elif x == 5:
+    price = 1.50
+    
+total = price * y
+print(f"Total: R$ {total}")    
+    
